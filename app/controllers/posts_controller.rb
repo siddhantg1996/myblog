dc12dfd637c4cd9aba4e319bf71defd5e3453330
siddhantg1996 <siddhantgandhi@gmail.com>
2016-06-23 21:53:51 +0530

    table update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+	def index
+	@content_first = 'This is some sample text i created-sidddhant';
+	@content_second = 'This is another set of text-sidddhant';
+	end
+
+	def new
+	
+	end
+	def create
+	
+	end 
+	
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def show
+	
+	end
+	
+	def destroy
+
+	end
+end
